909d14df7f0f7efd48ec139be67564c8583f4e15
Linh Nguyễn <nguyenhalinh@MacBook-Air-cua-Linh.local>
2023-01-10 12:53:04 +0700

    update1

---
+++ src/app/firstview/firstview.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-firstview',
+  templateUrl: './firstview.component.html',
+  styleUrls: ['./firstview.component.css']
+
+})
+export class FirstviewComponent implements OnInit{
+  ngOnInit(): void {
+  }
+
+
+}
