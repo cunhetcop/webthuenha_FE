e0d34bf44bae78bf7994ef4206635bd43ec984ca
Viet0308 <phamviet0308@gmail.com>
2023-01-17 09:57:20 +0700

    up

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -13,7 +15,6 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
